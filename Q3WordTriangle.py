3e5dc6348206afebd76b7afaba3c6836787c7eaf
Thatcher Reidel <124378590+ThatcherReidel@users.noreply.github.com>
2023-05-09 13:47:52 +0000

    asheane

--- Q3WordTriangle.py
+++ Q3WordTriangle.py
@@ -1,10 +1,5 @@
 # Create a program that will prompt the user for a word, and return a 'word triangle'. For example, if the user types in the word "PYTHON", your program will output:
 # Pword = input("Enter a word: ")
-word = input("Enter a word: ")
-length = len(word)
-
-for i in range(length):
+word = input("Please enter a word: ")
+for i in range(len(word)):
     print(word[:i+1])
-    
-for i in range(length-1, 0, -1):
-    print(word[:i])
